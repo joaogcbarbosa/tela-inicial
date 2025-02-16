c553c05213a50fe9fc5ade2b55c528cba735fe87
joaogcbarbosa <joaogcb29@gmail.com>
2025-02-16 01:53:21 -0300

    Refactor: (rollback) only email needs alert

--- assets/js/sign-in.js
+++ assets/js/sign-in.js
@@ -3,42 +3,24 @@ import { usersArray } from "./local-storage.js"
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 const emailDiv = document.querySelector("#email-div")
-const passwordDiv = document.querySelector("#password-div")
 
 const emailAlert = document.createElement("span")
 emailAlert.textContent = "Email não cadastrado"
 emailAlert.style.color = "red"
 
-const checkUserInfo = (usersArrayColumn, e) => {
-    let infoDiv
-    let infoAlert
-
-    switch (usersArrayColumn) {
-        case(1):
-            infoDiv = emailDiv
-            infoAlert = emailAlert
-            break
-        case(2):
-            infoDiv = passwordDiv
-            infoAlert = passwordAlert
-            break
-    }
-
-    const entered = e.target.value
-    const found = usersArray.some(u => u[usersArrayColumn] === entered)
+const checkEmail = (e) => {
+    const enteredEmail = e.target.value
+    const found = usersArray.some(u => u[1] === enteredEmail)
 
     if (!found) {
-        if (!infoDiv.contains(infoAlert)) {
-            infoDiv.appendChild(infoAlert)
+        if (!emailDiv.contains(emailAlert)) {
+            emailDiv.appendChild(emailAlert)
         }
-    } else if (infoDiv.contains(infoAlert)) {
-        infoDiv.removeChild(infoAlert)
+    } else if (emailDiv.contains(emailAlert)) {
+        emailDiv.removeChild(emailAlert)
     }
 
-    entered.length === 0 ? infoDiv.removeChild(infoAlert) : null
+    enteredEmail.length === 0 ? emailDiv.removeChild(emailAlert) : null
 }
 
-const checkWhichField = e => e.target.id === email.id ? checkUserInfo(1, e) : checkUserInfo(2, e)
-
-email.addEventListener("input", checkWhichField)
-password.addEventListener("input", checkWhichField)
+email.addEventListener("input", checkEmail)
