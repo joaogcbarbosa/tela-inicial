886834a775325da2ba0e7faf377f4579a615c081
joaogcbarbosa <joaogcb29@gmail.com>
2025-02-15 23:42:32 -0300

    Refactor: using generic function

--- assets/js/sign-in.js
+++ assets/js/sign-in.js
@@ -2,7 +2,7 @@ const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 const emailDiv = document.querySelector("#email-div")
 const passwordDiv = document.querySelector("#password-div")
-const users = [[0, "joaogcb29@gmail.com", "123456"]]
+const usersArray = [[0, "joaogcb29@gmail.com", "123456"]]
 
 const emailAlert = document.createElement("span")
 emailAlert.textContent = "Email não cadastrado"
@@ -12,30 +12,30 @@ const passwordAlert = document.createElement("span")
 passwordAlert.textContent = "Senha incorreta"
 passwordAlert.style.color = "red"
 
-const checkEmail = (e) => {
-    const emailAddress = e.target.value
-    const emailFound = users.some(u => u[1] === emailAddress)
+const checkUserInfo = (usersArrayColumn, e) => {
+    let infoDiv
+    let infoAlert
+    if (usersArrayColumn === 1) {
+        infoDiv = emailDiv
+        infoAlert = emailAlert
+    } else {
+        infoDiv = passwordDiv
+        infoAlert = passwordAlert
+    }
 
-    if (!emailFound) {
-        if (!emailDiv.contains(emailAlert)) {
-            emailDiv.appendChild(emailAlert)
+    const entered = e.target.value
+    const found = usersArray.some(u => u[usersArrayColumn] === entered)
+    
+    if (!found) {
+        if (!infoDiv.contains(infoAlert)) {
+            infoDiv.appendChild(infoAlert)
         }
-    } else if (emailDiv.contains(emailAlert)) {
-        emailDiv.removeChild(emailAlert)
+    } else if (infoDiv.contains(infoAlert)) {
+        infoDiv.removeChild(infoAlert)
     }
 }
 
-const checkPassword = (e) => {
-    const enteredPasswd = e.target.value
-    const passwdFound = users.some(u => u[2] === enteredPasswd)
-    if (!passwdFound) {
-        if (!passwordDiv.contains(passwordAlert)) {
-            passwordDiv.appendChild(passwordAlert)
-        }
-    } else if (passwordDiv.contains(passwordAlert)) {
-        passwordDiv.removeChild(passwordAlert)
-    }
-}
+const checkWhichField = e => e.target.id === email.id ? checkUserInfo(1, e) : checkUserInfo(2, e)
 
-email.addEventListener("input", checkEmail)
-password.addEventListener("input", checkPassword)
+email.addEventListener("input", checkWhichField)
+password.addEventListener("input", checkWhichField)
