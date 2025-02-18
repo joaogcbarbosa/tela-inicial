4610c28a69c70c5ff615cbc7d7cc724744cb7df5
joaogcbarbosa <joaogcb29@gmail.com>
2025-02-17 21:54:58 -0300

    Refactor: handling local storage

--- assets/js/local-storage.js
+++ assets/js/local-storage.js
@@ -1,1 +1,6 @@
-localStorage.setItem("usersArray", JSON.stringify([]))
+document.addEventListener("DOMContentLoaded", () => {
+    let usersArray = localStorage.getItem("usersArray")
+    if (!usersArray || usersArray === "null" || usersArray === "undefined") {
+        localStorage.setItem("usersArray", JSON.stringify([]))
+    }
+})
